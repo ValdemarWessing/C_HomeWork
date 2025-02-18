eab8c6e999489d30f7146dfa11cbb89a286a0419
ValdemarWessing <iq62FM@student.aau.dk>
2025-02-18 18:44:58 +0100

    add.git

--- Ch_11_Starter/Assets/Scripts/BaseManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class BaseManager : MonoBehaviour
-{
-    protected string _state = "Manager is not initialized...";
-    public abstract string State { get; set; }
-
-    public abstract void Initialize();
-}
